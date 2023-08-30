fab57080c4bc33089c37bec5563438564ba6ef7d
AMillsy <00adammills00@gmail.com>
2023-08-30 16:17:15 +0100

    converting the auth to graphql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,25 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Book {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+
+  type Query {
+    me: User
+  }
+`;
 
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -11,16 +11,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+//app.use(routes);
 
-/*const startApolloServer = async () => {
+const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
   db.once("open", () => {
@@ -34,4 +34,3 @@ app.use(routes);
 };
 
 startApolloServer();
-*/

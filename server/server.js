b84c85ac8f52df5b3df061421c9044ee834f0ebf
AMillsy <00adammills00@gmail.com>
2023-08-30 15:59:36 +0100

    set up the server for when graphql is implemented

--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
 const { typeDefs, resolvers } = require("./schemas");
+const { ApolloServer } = require("apollo-server-express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//const server = new ApolloServer({ typeDefs, resolvers });
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
@@ -17,6 +20,18 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+/*const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(
+        `Use GraphQL ast http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+startApolloServer();
+*/
